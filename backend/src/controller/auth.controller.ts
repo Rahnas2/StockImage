fb3fa3ee91a54361f2c13637cf8ae805cdc9751f
Rahnas p <rahnasx2@gmail.com>
2025-05-27 17:18:22 +0530

    chagne lax to none

--- backend/src/controller/auth.controller.ts
+++ backend/src/controller/auth.controller.ts
@@ -17,7 +17,7 @@ export class AuthController {
             res.cookie('refreshToken', refreshToken, {
                 httpOnly: true,
                 secure: process.env.NODE_ENV  === 'production',
-                sameSite: 'lax',
+                sameSite: 'none',
                 maxAge: 1 * 24 * 60 * 60 * 1000,
             })
 
