4aa958ee85685ed0d0b0114b99cdde5fddc90bbd
Rahnas p <rahnasx2@gmail.com>
2025-06-03 09:38:55 +0530

    issue: not clearing token on logout

--- backend/src/controller/auth.controller.ts
+++ backend/src/controller/auth.controller.ts
@@ -1,14 +1,14 @@
-import { Request, Response ,NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
 import { AuthService } from "../services/auth.service";
 import { HttpStatusCode } from "../utils/statusCode";
 import { UnauthorizedError } from "../utils/errors";
 
 export class AuthController {
-    constructor(private authService: AuthService) {}
+    constructor(private authService: AuthService) { }
 
     login = async (req: Request, res: Response, next: NextFunction) => {
         try {
-            const {email, password} = req.body
+            const { email, password } = req.body
 
             const { accessToken, refreshToken } = await this.authService.login(email, password)
             console.log('refresh token after login ', refreshToken)
@@ -16,97 +16,101 @@ export class AuthController {
             console.log('node env ', process.env.NODE_ENV)
             res.cookie('refreshToken', refreshToken, {
                 httpOnly: true,
-                secure: process.env.NODE_ENV  === 'production',
+                secure: process.env.NODE_ENV === 'production',
                 sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
                 maxAge: 1 * 24 * 60 * 60 * 1000,
             })
 
-            res.status(HttpStatusCode.OK).json({accessToken})
+            res.status(HttpStatusCode.OK).json({ accessToken })
 
         } catch (error) {
             next(error)
         }
     }
 
-    register = async(req: Request, res: Response, next: NextFunction) => {
+    register = async (req: Request, res: Response, next: NextFunction) => {
         try {
-            const {name, email, mobile, password} = req.body
+            const { name, email, mobile, password } = req.body
 
-            const image = req.file 
+            const image = req.file
 
             await this.authService.register(name, email, mobile, password, image)
 
-            res.status(HttpStatusCode.CREATED).json({message: `We have sent a verification link to your email (${email}). Please verify it before logging in.`})
+            res.status(HttpStatusCode.CREATED).json({ message: `We have sent a verification link to your email (${email}). Please verify it before logging in.` })
         } catch (error) {
             next(error)
         }
     }
 
-    verifyEmail = async(req: Request, res: Response, next: NextFunction) => {
+    verifyEmail = async (req: Request, res: Response, next: NextFunction) => {
         try {
             const { token } = req.query
 
             await this.authService.verifyEmail(token as string)
             res.redirect(HttpStatusCode.REDIRECT, `${process.env.CLIENT_URI}/email-verified?status=success&message=verified`)
-        } catch (error) {   
+        } catch (error) {
             res.redirect(HttpStatusCode.REDIRECT, `${process.env.CLIENT_URI}/email-verified?status=failed&message=failed`)
         }
     }
 
-    sendOtp = async(req: Request, res: Response, next: NextFunction) => {
+    sendOtp = async (req: Request, res: Response, next: NextFunction) => {
         try {
             const { email } = req.body
 
             await this.authService.sendOtp(email)
 
-            res.status(HttpStatusCode.OK).json({message: 'success'})
+            res.status(HttpStatusCode.OK).json({ message: 'success' })
         } catch (error) {
             next(error)
         }
     }
 
-    verifyOtp = async(req: Request, res: Response, next: NextFunction) => {
+    verifyOtp = async (req: Request, res: Response, next: NextFunction) => {
         try {
-            const {email, otp} = req.body
+            const { email, otp } = req.body
 
             await this.authService.verifyOtp(email, otp)
-            res.status(HttpStatusCode.OK).json({message: 'success'})
+            res.status(HttpStatusCode.OK).json({ message: 'success' })
         } catch (error) {
             next(error)
         }
     }
 
-    resetPassword = async(req: Request, res: Response, next: NextFunction) => {
+    resetPassword = async (req: Request, res: Response, next: NextFunction) => {
         try {
             const { otp, email, newPassword } = req.body
 
-            await this.authService.resetPassword(otp, email, newPassword)     
-            res.status(HttpStatusCode.OK).json({message: 'success'})
+            await this.authService.resetPassword(otp, email, newPassword)
+            res.status(HttpStatusCode.OK).json({ message: 'success' })
         } catch (error) {
             next(error)
         }
     }
 
-    refreshToken = async(req: Request, res: Response, next: NextFunction) => {
+    refreshToken = async (req: Request, res: Response, next: NextFunction) => {
         try {
-            const refreshToken = req.cookies.refreshToken 
+            const refreshToken = req.cookies.refreshToken
             console.log('refesh token', refreshToken)
-            if(!refreshToken) {
+            if (!refreshToken) {
                 throw new UnauthorizedError('Unauthorized')
             }
 
             const accessToken = await this.authService.refreshToken(refreshToken)
 
-            res.status(HttpStatusCode.OK).json({message: 'success', accessToken})
+            res.status(HttpStatusCode.OK).json({ message: 'success', accessToken })
         } catch (error) {
             next(error)
         }
     }
 
-    logout = async(req: Request, res: Response, next: NextFunction) => {
+    logout = async (req: Request, res: Response, next: NextFunction) => {
         try {
-            res.clearCookie('refreshToken')
-            res.status(HttpStatusCode.OK).json({message: 'success'})
+            res.clearCookie('refreshToken', {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+            })
+            res.status(HttpStatusCode.OK).json({ message: 'success' })
         } catch (error) {
             next(error)
         }
